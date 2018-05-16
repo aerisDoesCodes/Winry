c586dff09690228e15e452f8cc450b638aed5522
aerisDoesCodes <aerisdoescodes@gmail.com>
2018-05-16 18:56:44 +0800

    woooh penis

--- commands/penis.js
+++ commands/penis.js
@@ -4,6 +4,7 @@ exports.run = async (client, message, args, level) => { // eslint-disable-line n
   const penis = `8${"=".repeat((Math.floor(Math.random() * 10) + 1 ) * 2)}D`
      // const content = message.content.split(' ').slice(1).join(' ');
      //   if (!content) return message.reply("Gimme somethin to nut mate!");
+     if (message.mentions.users.size === 0) {
        jimp.read('https://cdn.glitch.com/aface8e8-5406-40cd-b4ab-f54eaa7496c7%2FPenis%20Size%201.png?1526464771236', (err, image) => {
          message.channel.send(':gear: generating...').then(async (msg) => {msg.delete(5000)})
          if (err) return console.log(err);
@@ -21,6 +22,25 @@ exports.run = async (client, message, args, level) => { // eslint-disable-line n
            });
          });
        });
+     } else {
+       jimp.read('https://cdn.glitch.com/aface8e8-5406-40cd-b4ab-f54eaa7496c7%2FPenis%20Size%201.png?1526464771236', (err, image) => {
+         message.channel.send(':gear: generating...').then(async (msg) => {msg.delete(5000)})
+         if (err) return console.log(err);
+         const text = new jimp(800, 150, function(err, text) {
+           if (err) return console.log(err);
+           jimp.loadFont(jimp.FONT_SANS_64_BLACK).then(function(font) {
+             // text.resize(200, 200)
+              text.print(font, 0, 0, penis, 10000);
+              image.print(font, 30, 180, `${message.mentions.users.first().username}\'s Penis Size:`, 650);
+              image.composite(text, 17, 450)
+              image.getBuffer(jimp.AUTO, (err, buffer) => {
+                if (err) return console.log(err);
+                message.channel.sendFile(buffer).catch((err) => {message.channel.send(`:warning: **An error occurred.**\n\`\`\`js\n${err.stack}\`\`\``); console.log(err)});
+              })
+           });
+         });
+       });
+     }
 };
 
 exports.conf = {
