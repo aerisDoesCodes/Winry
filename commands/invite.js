a413955dd9584fabd9a77cab3d7f87eca76d03b6
aerisDoesCodes <aerisdoescodes@gmail.com>
2018-05-17 10:32:26 +0800

    Invite updooted

--- commands/invite.js
+++ commands/invite.js
@@ -1,5 +1,5 @@
 exports.run = async (client, message, args, level) => { // eslint-disable-line no-unused-vars
-message.channel.send("Well oh my, you like me don't ya?\nHere is my invite <https://discordapp.com/oauth2/authorize?client_id=442917513454682122&permissions=201386057&scope=bot>")
+message.channel.send("Well oh my, you like me don't ya?\nHere is my invite <https://discordapp.com/oauth2/authorize?client_id=442917513454682122&permissions=201386057&scope=bot>\nJoin my Just a bot support ok? https://discord.gg/HWEMcEY")
 };
 
 exports.conf = {
