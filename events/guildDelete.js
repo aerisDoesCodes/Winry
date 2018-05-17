4264915308eff8ef4a44d7a204e48010614613fc
aerisDoesCodes <aerisdoescodes@gmail.com>
2018-05-17 21:52:41 +0800

    ok

--- events/guildDelete.js
+++ events/guildDelete.js
@@ -1,7 +1,7 @@
 // This event executes when a new guild (server) is left.
 
 module.exports = (client, message, guild) => {
-  client.user.setActivity(`${client.config.defaultSettings.prefix}help || ${client.guilds.size}`, {type: "PLAYING"});
+  client.user.setActivity(`${client.config.defaultSettings.prefix}help || ${client.guilds.size} Servers`, {type: "PLAYING"});
 
   // If the settings Enmap contains any guild overrides, remove them.
   // No use keeping stale data!
