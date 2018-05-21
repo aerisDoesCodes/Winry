81f4d661eaf386dca344def0455ef859c0f645a5
aerisDoesCodes <aerisdoescodes@gmail.com>
2018-05-22 00:10:10 +0800

    New UI

--- commands/help.js
+++ commands/help.js
@@ -5,8 +5,6 @@ command is also filtered by level, so if a user does not have access to
 a command, it is not shown to them. If a command name is given with the
 help command, its extended help is shown.
 */
-const Discord = require("discord.js");
-const embed = new Discord.RichEmbed();
 const cooldown = new Set();
 
 exports.run = (client, message, args, level) => {
@@ -33,17 +31,18 @@ exports.run = (client, message, args, level) => {
     //   output += `\`${message.settings.prefix}${c.help.name}\` `;
     // });
     // message.channel.send(output + "\n\nA very cool invite link: https://invite.gg/justabot")
-    const pr = message.settings.prefix
+    const Discord = require("discord.js");
+    const embed = new Discord.RichEmbed();
+    const pr = "w!";
     embed.addField("Help Command", "[Support Server](https://discord.gg/HWEMcEY) | [Bot Invite](https://discordapp.com/oauth2/authorize?client_id=442917513454682122&permissions=201386057&scope=bot)"+
-    "\n**Server Prefix:** ${message.settings.prefix}\n**More Help:** ${message.settings.prefix}help <commandName>")
+    "\n**Server Prefix:** w!\n**More Help:** w!help <commandName>")
     .addField('<:ImageManipulation:448118659668246529> Image Manipulation', `\`${pr}achievement\`, \`${pr}bug\`, \`${pr}error\`, \`${pr}news\`, \`${pr}nut\`, \`${pr}penis\`, \`${pr}step\`, \`${pr}tobe\``)
     .addField('<:ImageManipulation:448118659668246529> Images', `\`${pr}anime\``)
     .addField('<:utility:423310165417263104> Utility', `\`${pr}info\`, \`${pr}invite\`, \`${pr}mylevel\`, \`${pr}ping\`, \`${pr}stats\`​`)
     .addField('<:Cumdrizzle:448119014909280266> NSFW', `\`${pr}ass\`, \`${pr}boobs\`, \`${pr}neko\`, \`${pr}pantsu\`, \`${pr}pussy\`​`)
     .addField('<:stafftools:448118904569593866> System', `\`${pr}eval\`, \`${pr}help\`, \`${pr}post\`, \`${pr}reboot\`, \`${pr}reload\`, \`${pr}set\` ​`)
     .setColor('#f1f199')
-    message.channel.send({embed})
-
+    message.channel.send({embed});
   } else {
     // Show individual command's help.
     let command = args[0];
@@ -51,8 +50,8 @@ exports.run = (client, message, args, level) => {
       command = client.commands.get(command);
       if (level < client.levelCache[command.conf.permLevel]) return;
       message.channel.send(`**Name:** ${command.help.name}\n**Description:** ${command.help.description}\n**Usage:** ${command.help.usage}\n**Aliases**: ${command.conf.aliases.join(", ")}`);
-    }
-  }
+    };
+  };
   cooldown.add(message.author.id);
      setTimeout(() => {
        cooldown.delete(message.author.id);
