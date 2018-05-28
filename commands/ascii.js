4f367522dc0ed19832edf3a40c72aba479b1cc64
aerisDoesCodes <aerisdoescodes@gmail.com>
2018-05-28 20:53:03 +0800

    Winry v3.5.1

--- commands/ascii.js
+++ commands/ascii.js
@@ -2,11 +2,11 @@ const cooldown = new Set();
 const figletAsync = require('util').promisify(require('figlet'));
 
 exports.run = async (client, message, level) => {
-//   if (cooldown.has(message.author.id)) {
-//        return message.channel.send(`**${message.author.username}, please cool down! (3 seconds)**`).then(m => {
-//          m.delete(10000)
-//        });
-//       }
+  if (cooldown.has(message.author.id)) {
+       return message.channel.send(`**${message.author.username}, please cool down! (3 seconds)**`).then(m => {
+         m.delete(10000)
+       });
+      }
 const args = message.content.split(' ').slice(1).join(' ');
 
 if (!args) return message.reply("Please give me something to say!");
@@ -14,22 +14,21 @@ if (args.length > '15') return message.reply("You must not exceed more than 15 c
 const data = await figletAsync(args);
 return message.channel.send(data, { code: true });
 
-//   cooldown.add(message.author.id);
-//      setTimeout(() => {
-//        cooldown.delete(message.author.id);
-//      }, 3000);
-// };
-}
+  cooldown.add(message.author.id);
+     setTimeout(() => {
+       cooldown.delete(message.author.id);
+     }, 3000);
+};
 exports.conf = {
   enabled: true,
-  guildOnly: false,
+  guildOnly: true,
   aliases: [],
-  permLevel: "Bot Owner"
+  permLevel: "User"
 };
 
 exports.help = {
-  name: "test",
-  category: "ok",
-  description: "Not sure what should I add here.",
-  usage: "test [ok]"
+  name: "ascii",
+  category: "Fun",
+  description: "Convert text to ascii.",
+  usage: "ascii [text]"
 };
--- commands/updatelogs.js
+++ commands/updatelogs.js
@@ -1,5 +1,4 @@
 const cooldown = new Set();
-const ver = "Winry v3.4.1"
 
 exports.run = (client, message, args, level) => {
   if (cooldown.has(message.author.id)) {
@@ -7,9 +6,17 @@ exports.run = (client, message, args, level) => {
          m.delete(10000)
        });
       }
-      const currentUpdate = [
+
+      const May282018 = [
         "\`\`\`diff"+
-        `\n${ver}\n`+
+        "\nWinry v3.5.1\n"+
+        "\n+ Added Fun Command \"w!ascii\" on Fun Category"+
+        "\`\`\`"
+      ]
+
+      const May272018 = [
+        "\`\`\`diff"+
+        "\nWinry v3.4.1\n"+
         "\n+ Added NSFW Commands \"w!rule34 and w!snap\""+
         "\n+ Added \"w!updatelogs\" command to stay updated"+
         "\n+ Fix typo description"+
@@ -18,13 +25,16 @@ exports.run = (client, message, args, level) => {
         "\`\`\`"
         ]
 
-        const previousUpdate = [
+        const May252018 = [
           "\`\`\`diff"+
           "\n+ Added NSFW Command \"w!teen\""+
           "\`\`\`"
           ]
 
-      message.channel.send("**May 27 2018 Updates:**\n" + currentUpdate + "\n\n" + "**May 25 2018 Update:**\n" + previousUpdate)
+      message.channel.send(
+      "**May 28 2018 Update:**\n" + May282018 +
+      "\n\n**May 27 2018 Updates:**\n" + May272018 + 
+      "\n\n" + "**May 25 2018 Update:**\n" + May252018)
   cooldown.add(message.author.id);
      setTimeout(() => {
        cooldown.delete(message.author.id);
