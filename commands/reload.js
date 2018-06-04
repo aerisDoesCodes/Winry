20105d9df46c0207fb5439f0a9deebcbbe276fc1
aerisDoesCodes <aerisdoescodes@gmail.com>
2018-06-04 19:31:01 +0800

    alias tbh

--- commands/reload.js
+++ commands/reload.js
@@ -13,7 +13,7 @@ exports.run = async (client, message, args, level) => {// eslint-disable-line no
 exports.conf = {
   enabled: true,
   guildOnly: false,
-  aliases: [],
+  aliases: ['r'],
   permLevel: "Bot Admin"
 };
 
