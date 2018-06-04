6e09c11f300f50f458d88fab3d55d30489ccceb6
aerisDoesCodes <aerisdoescodes@gmail.com>
2018-06-04 15:43:26 +0800

    playing status

--- events/ready.js
+++ events/ready.js
@@ -1,6 +1,6 @@
 const DBL = require("dblapi.js");
 const dbl = new DBL('eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjQ0MjkxNzUxMzQ1NDY4MjEyMiIsImJvdCI6dHJ1ZSwiaWF0IjoxNTI2ODY0MDk1fQ.qpD1MaIePeW8I6LbcqqdEqm5tEFgThIKTX87bkX9YSY');
-const games = ["w!help || w!upvote", "abc", "xyz"];
+const games = ["w!upvote || w!help", "abc || w!help", "xyz || w!help", "discord.gg/HWEMcEY || w!help"];
 
 module.exports = async client => {
   // Log that the bot is online.
