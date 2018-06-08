0c5f72839d360b3babb0d78a348548d4f9e88d4d
aerisDoesCodes <aerisdoescodes@gmail.com>
2018-06-08 16:54:04 +0800

    random text on ping command

--- commands/ping.js
+++ commands/ping.js
@@ -1,6 +1,20 @@
 exports.run = async (client, message, args, level) => { // eslint-disable-line no-unused-vars
+  const randomText = [
+    "What are you doing?",
+    "Am I even a thing?",
+    "OwO what is this?",
+    "Are you a bot?",
+    "Noticed me senpai!",
+    "Does people even use this?",
+    "Is this thing even working?",
+    "Who are you?",
+    "Why should I even do this?",
+    "A thing is a thing!",
+    "Yes, I am alive!",
+    "uh?"
+  ]
   const msg = await message.channel.send("Ping?");
-  msg.edit(`Pong! Latency is ${msg.createdTimestamp - message.createdTimestamp}ms. API Latency is ${Math.round(client.ping)}ms`);
+  msg.edit(`**${randomText[Math.floor(Math.random() * randomText.length)]}**` + ' - ' + `Pong! ${Math.round(client.ping)}ms`);
 };
 
 exports.conf = {
