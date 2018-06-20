6f264d8f5cd07105322790fdf9b53bceb081d25e
aerisDoesCodes <aerisdoescodes@gmail.com>
2018-06-20 11:49:11 +0800

    Typo

--- commands/ascii.js
+++ commands/ascii.js
@@ -4,7 +4,7 @@ const figletAsync = require('util').promisify(require('figlet'));
 exports.run = async (client, message, level) => {
   const pr = `${message.settings.prefix}`;
   const  usageText = "The `text` argument is required.\n"+
-  `Command Usage: \`${pr}achievement <text>\``
+  `Command Usage: \`${pr}ascii <text>\``
 
   if (cooldown.has(message.author.id)) {
        return message.channel.send(`**${message.author.username}, please cool down! (3 seconds)**`).then(m => {
